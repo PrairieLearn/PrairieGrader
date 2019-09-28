06482c54f402af5bbcdaa79323d86bff6d62881d
Matthew West <mwest@illinois.edu>
2019-09-28 12:20:32 -0500

    report lifecycle and config

--- lib/load.sql
+++ lib/load.sql
@@ -1,11 +1,23 @@
 -- BLOCK insert_load
 INSERT INTO grader_loads
-    ( instance_id,  queue_name,  average_jobs,  max_jobs)
+    ( instance_id, date,   queue_name,  average_jobs,  max_jobs,  lifecycle_state,  healthy)
 VALUES
-    ($instance_id, $queue_name, $average_jobs, $max_jobs)
+    ($instance_id, now(), $queue_name, $average_jobs, $max_jobs, $lifecycle_state, $healthy)
 ON CONFLICT (instance_id) DO UPDATE
 SET
     date = now(),
     queue_name = EXCLUDED.queue_name,
     average_jobs = EXCLUDED.average_jobs,
-    max_jobs = EXCLUDED.max_jobs;
+    max_jobs = EXCLUDED.max_jobs,
+    lifecycle_state = EXCLUDED.lifecycle_state,
+    healthy = EXCLUDED.healthy;
+
+-- BLOCK insert_config
+INSERT INTO grader_loads
+    ( instance_id,  config, started_at)
+VALUES
+    ($instance_id, $config, now())
+ON CONFLICT (instance_id) DO UPDATE
+SET
+    config = EXCLUDED.config,
+    started_at = now();

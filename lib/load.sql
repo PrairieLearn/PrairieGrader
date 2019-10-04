fe65a6b436d52b7854b6d2a2d406dcf2a1aae2d1
Matthew West <mwest@illinois.edu>
2019-10-04 18:46:30 -0500

    set all required columns in grader_loads

--- lib/load.sql
+++ lib/load.sql
@@ -1,8 +1,8 @@
 -- BLOCK insert_load
 INSERT INTO grader_loads
-    ( instance_id, date,   queue_name,  average_jobs,  max_jobs,  lifecycle_state,  healthy)
+    ( instance_id,  queue_name,  average_jobs,  max_jobs,  lifecycle_state,  healthy)
 VALUES
-    ($instance_id, now(), $queue_name, $average_jobs, $max_jobs, $lifecycle_state, $healthy)
+    ($instance_id, $queue_name, $average_jobs, $max_jobs, $lifecycle_state, $healthy)
 ON CONFLICT (instance_id) DO UPDATE
 SET
     date = now(),
@@ -14,10 +14,14 @@ SET
 
 -- BLOCK insert_config
 INSERT INTO grader_loads
-    ( instance_id,  config, started_at)
+    ( instance_id,  queue_name,  average_jobs,  max_jobs,  config, started_at)
 VALUES
-    ($instance_id, $config, now())
+    ($instance_id, $queue_name, $average_jobs, $max_jobs, $config, now())
 ON CONFLICT (instance_id) DO UPDATE
 SET
+    date = now(),
+    queue_name = EXCLUDED.queue_name,
+    average_jobs = EXCLUDED.average_jobs,
+    max_jobs = EXCLUDED.max_jobs,
     config = EXCLUDED.config,
-    started_at = now();
+    started_at = EXCLUDED.started_at;
